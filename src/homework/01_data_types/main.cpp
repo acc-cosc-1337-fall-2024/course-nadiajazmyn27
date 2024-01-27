8d57a4dc59f1e25bfacc90cff64e0a3b37114a26
nadiajazmyn27 <157078469+nadiajazmyn27@users.noreply.github.com>
2024-01-27 23:41:51 +0000

    commit

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,12 +1,20 @@
 ﻿//write include statements
-
+#include <isostream>
 
 //write namespace using statement for cout
-
-
+using std::count;
+using std::cin;
 int main()
 {
-	
+	int num = 0;
+
+	cout<<"Enter a number: ";
+	cin>>num;//value from keyboard saved into the num variable
+
+int result = 0;
+result = multiply_numbers(num);
+
+cout<<"value is: "<<result<<"\n";
 
 	return 0;
 }
