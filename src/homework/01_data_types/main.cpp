b36fe57645d2dac1e9afd769958959b941640a57
nadiajazmyn27 <157078469+nadiajazmyn27@users.noreply.github.com>
2024-02-18 04:41:55 +0000

    HW 3 finished and HW2 edited

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,11 +1,7 @@
 ﻿//write include statements
 #include <iostream>
-
+#include  "data_types.h"
 //write namespace using statement for cout
-
-int multiply_numbers(int num) {
-	return num * 2;
-}
 
 int main()
 {
@@ -20,7 +16,7 @@ std::cout<<"Value is: " << result << "\n";
 int num1 = 4;
 
 result = multiply_numbers(num1);
-std::cout << "value is: " << result << "n";
+std::cout << "value is: " << result << "\n";
 
 	return 0;
 }
--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,32 @@
 //write include statements
 
+#include "decisions.h"
+#include <iostream>
+
 int main() 
 {
+	int option, grade;
+	std::cout << "MAIN MENU\n\n1-Letter grade using if\n2-Letter grade using switch\n3-Exit\n\n";
+	std::cin >> option;
+
+	if(option == 1 || option == 2) {
+		std::cout <<"Enter a numerical grade (0-100): ";
+		std::cin >> grade;
+		if (grade < 0 || grade > 100) {
+			std::cout << "Number is out of range. \n";
+	    } else {
+			std::string result = (option == 1) ? get_letter_grade_using_if(grade) : get_letter_grade_using_switch(grade);
+			std::cout << "The letter grade is: " << result << std::endl;
+		}
+	} else if (option == 3) {
+		std::cout << "Exciting...\n";
+	} else {
+			std::cout << "Invalid option. \n";
+	}
+	
+
+
+
+
 	return 0;
 }
